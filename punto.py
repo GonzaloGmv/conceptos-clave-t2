63fc8f873f5fb567bf9f42a481dbd980a0a145f9
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-09-21 16:48:40 +0200

    cuadrante

--- punto.py
+++ punto.py
@@ -1,10 +1,26 @@
 class punto():
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def string(self):
         print("(", self.x, ",", self.y, ")")
-    
 
+    def cuadrante(self):
+        if self.x == 0 and self.y == 0:
+            print("Esta en el origen")
+        elif self.x == 0 and self.y != 0:
+            print("Esta en el eje Y")
+        elif self.y == 0 and self.x != 0:
+            print("Esta en el eje X")
+        elif self.y > 0 and self.x > 0:
+            print("Está en el primer cuadrante")
+        elif self.y > 0 and self.x < 0:
+            print("Está en el segundo cuadrante")
+        elif self.y < 0 and self.x < 0:
+            print("Está en el tercer cuadrante")
+        elif self.y < 0 and self.x > 0:
+            print("Está en el cuarto cuadrante")
 
 punto(1,2).string()
